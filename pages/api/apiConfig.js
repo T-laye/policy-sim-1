719cfc3311d15b1195eb6e2f06af978051bee60e
Laye <108472848+T-laye@users.noreply.github.com>
2024-09-27 12:54:43 +0000

    test

--- pages/api/apiConfig.js
+++ pages/api/apiConfig.js
@@ -1,5 +1,3 @@
-
-// module.exports
  export const apiConf = {
   apiKey: process.env.HUGGING_FACE_API_KEY,  // Load from environment variable
   apiUrl: `https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-1.3B`,
